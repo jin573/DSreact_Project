36d3e8a3a2a2a90a9533d518c8d04696b2335719
JinSeon Kim <rlawlstjs5573@gmail.com>
2025-05-11 02:56:56 +0900

    수정 기능 구현#4

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import Book from "./Book";
 import AddBook from "./AddBook";
 import GetBook from './GetBook';
+import UpdateBook from './UpdateBook';
 import "./App.css";
 import React, { useEffect, useState } from "react";
 import { call } from "./service/ApiService";
@@ -8,6 +9,8 @@ import { call } from "./service/ApiService";
 function App() {
   const [items, setItems] = useState([]); //책을 등록하여서 테이블에 추가할 때 사용
   const [getItem, getItems] = useState([]); //검색 할 때 사용
+  const [updategetItem, updategetItems] = useState([]); //수정 할 때 사용할 검색
+  const [updateItem, updateItems] = useState([]); //수정 할 때 사용
 
   useEffect(() => {
     call("/book", "GET", null).then((response) => setItems(response.data));
@@ -22,6 +25,18 @@ function App() {
       title: item.title
     }).toString();
     call(`/book/search?${params}`, "GET").then((response) =>{console.log(response.data); getItems(response.data)});
+  };
+
+  const getBookbyUpdate = (item) => {
+    const params = new URLSearchParams({
+      title: item.title
+    }).toString();
+    call(`/book/search?${params}`, "GET").then((response) =>{console.log(response.data); updategetItems(response.data)});
+  }
+
+  const putItem = (item) => {
+    call("/book", "PUT", item).then((response) =>{updateItems(response.data)});
+    call("/book", "GET", null).then((response) => setItems(response.data));
   };
 
   //테이블에 보여줄 책 객체가 있어야 함
@@ -48,6 +63,10 @@ function App() {
   let searchItems = getItem[0];
   console.log(searchItems);
 
+  let getBookbyUpdateITems = updategetItem[0];
+  let updateitem = updateItem[0];
+
+
   return (
     <div className="App">
       {/**현재 제품 리스트를 보여주는 UI */}
@@ -61,6 +80,10 @@ function App() {
       {/**제품 검색 */}
       <h1>Book search</h1>
         <GetBook searchItem={searchItem} searchResults={searchItems} />
+
+        {/**제품 수정 */}
+      <h1>Book update</h1>
+        <UpdateBook getBookbyUpdate = {getBookbyUpdate} updateItem={putItem} searchResults={getBookbyUpdateITems} updateitem={updateitem}/>
     </div>
   );
 }
---
+++ src/UpdateBook.js
@@ -0,0 +1,96 @@
+import React, { useState, useEffect } from 'react';
+import { Box, Stack, TextField, Button } from "@mui/material";
+
+const UpdateBook = (props) => {
+const [inputItem, getItems] = useState({title: "", author: "", publisher: "", userId: "" }); //초기값
+    const searchItem = props.getBookbyUpdate; //부모에 정의되어있는 searchItems 함수
+    const result = props.searchResults;
+    const updateItem = props.updateItem;
+
+    useEffect(() => {
+        if (result) {
+            getItems((prev) => ({
+                ...prev,
+                id: result.id,
+                title: result.title || "",
+                author: result.author || "",
+                publisher: result.publisher || "",
+                userId: result.userId || ""
+            }));
+        }
+    }, [result]);
+
+    //조회 버튼 클릭 시
+    const onButtonClick = (e) => {
+        if(e.target.name === "search"){
+            searchItem(inputItem);//App.js로 inputItem(title)을 넘겨줘야 한다.
+        } else if(e.target.name === "update"){
+            console.log("수정 요청:", inputItem);
+            updateItem(inputItem);
+        }
+        
+    };
+
+    //title 검색 시
+    const onInputChange = (e) => {
+        const { name, value } = e.target;
+        getItems((prev) => ({
+        ...prev,
+        [name]: value
+    }));
+    };
+
+    return (
+        <Stack direction="column" spacing={2} alignItems="center">
+                <Box sx={{ width: 500 }}>
+                    <Stack
+                        direction="row"
+                        spacing={2}
+                        alignItems="center"
+                        justifyContent="flex-start"
+                    >
+                        <Box sx={{ minWidth: "80px", textAlign: "left" }}>
+                        <h5>title:</h5>
+                        </Box>
+                        <TextField size="small" name="title" onChange={onInputChange} value={inputItem.title}/>
+                        <Button size="small" variant="contained" onClick={onButtonClick} name="search"> 
+                        {" "}
+                        제품 검색{" "}
+                        </Button>
+                    </Stack>
+            
+                    <Stack direction="row" spacing={2} alignItems="center">
+                        <Box sx={{ minWidth: "80px", textAlign: "left" }}>
+                        <h5>author:</h5>
+                        </Box>
+                        <TextField size="small" name="author" value={inputItem.author}/>
+                    </Stack>
+            
+                    <Stack direction="row" spacing={2} alignItems="center">
+                        <Box sx={{ minWidth: "80px", textAlign: "left" }}>
+                        <h5>publisher:</h5>
+                        </Box>
+                        <TextField size="small" name="publisher" value={inputItem.publisher}/>
+                    </Stack>
+            
+                    <Stack
+                        direction="row"
+                        spacing={2}
+                        alignItems="center"
+                        justifyContent="flex-start"
+                    >
+                        <Box sx={{ minWidth: "80px", textAlign: "left" }}>
+                        <h5>userId:</h5>
+                        </Box>
+                        <TextField size="small" name="userId" onChange={onInputChange} value={inputItem.userId}/>
+                        <Button size="small" variant="contained" onClick={onButtonClick} name="update"> 
+                        {" "}
+                        제품 수정{" "}
+                        </Button>
+                    </Stack>
+                </Box>
+            </Stack>
+    );
+}
+
+export default UpdateBook;
